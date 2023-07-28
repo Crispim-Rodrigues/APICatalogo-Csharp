20a554c0243a60e97b354deb2b6c59249c8e58b8
unknown <crispim.rodrigues.oficial@gmail.com>
2023-07-28 19:09:53 -0300

    Migration v2

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,22 @@
-﻿namespace APICatalogo.Models
+﻿using Microsoft.VisualBasic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Models
 {
     public class Categoria
     {
-        public int Id { get; set; } 
+        public Categoria() 
+        { 
+            Produtos = new Collection<Produto>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(80)]
         public string? Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string? ImageUrl { get; set; }
 
         public ICollection<Produto>? Produtos { get; set; }
