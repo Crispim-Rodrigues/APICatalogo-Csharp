e6911cca303bb667d467005f1cfb01e62c94b755
Crispim-Rodrigues <crispim.rodrigues.oficial@gmail.com>
2023-08-26 20:29:34 -0300

    Crud produto e categoria

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -20,7 +20,7 @@ namespace APICatalogo.Models
         public string? ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
-        public int CategotiaId { get; set; }
+        public int CategoriaId { get; set; }
         public Categoria? Categoria { get; set; }
     }
 }
